46d6fe955252baf9a5274ecc4cffd6de463ba000
Zen <1984.apple@gmail.com>
2020-06-17 17:25:36 +0800

    fix:forgot to merge the rest of commit

--- routes/members.js
+++ routes/members.js
@@ -53,7 +53,7 @@ router.get('/index/hot_cities', indexAction.showHotCities);
 router.get('/index/latest_posts', indexAction.showLatestPosts);
 router.get('/cities', cityAction.showCities);
 router.get('/cities/:id/towns', townAction.showTowns);
-router.get('/locations', locationAction.showLocations);
+router.get('/towns/:id/locations', locationAction.showLocations);
 router.get('/locations/:id/postmark', locationAction.showPostmark);
 router.get('/locations/:id/content', locationAction.showLocationInfo);
 router.get('/postmarks/:id', locationAction.showPostmarkInfo);
