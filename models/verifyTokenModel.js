f15a5a74d66f76c55d922fff87b4a028af25fc9b
Zen <1984.apple@gmail.com>
2020-06-29 15:25:06 +0800

    fix:fix when user request api without api_token, will remind user

--- models/verifyTokenModel.js
+++ models/verifyTokenModel.js
@@ -2,17 +2,17 @@ const { Users } = require('../connection_db')
 
 exports.tokenAuth = async function verifyToken(req, res, next) {
     const requestToken = req.headers.api_token;
-    const checkUser = await Users.findOne({ where: { api_token: requestToken } })
-
-    if (!requestToken) {
+    if (requestToken === undefined) {
         res.json({
-            err: "請輸入token！"
+            err: "請輸入api_token！"
         })
         return;
-    } else if (checkUser == null) {
+    }
+    const checkUser = await Users.findOne({ where: { api_token: requestToken } })
+    if (checkUser == null) {
         res.json({
             result: {
-                status: "token錯誤。",
+                status: "api_token錯誤。",
                 err: "請重新登入。"
             }
         })
