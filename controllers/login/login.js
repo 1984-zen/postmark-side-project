a85d2e75b22b3fc1c8a988c3dff586a867f15d2e
zen <1984.apple@gmail.comn>
2020-07-19 11:50:57 +0800

    refactor:login api

--- controllers/login/login.js
+++ controllers/login/login.js
@@ -1,5 +1,6 @@
-const { checkLogin } = require("../../models/usersModel");
+const { checkLogin, checkUser, updateUserToken } = require('../../models/usersModel');
 const crypto = require('crypto');
+const makeToken = require('../../models/makeTokenModel');
 
 module.exports = async function (req, res, next) {
     try {
@@ -7,25 +8,54 @@ module.exports = async function (req, res, next) {
             account: req.body.account,
             password: null,
         };
+        //hash password
         let hashPassword = crypto.createHash('sha1');
         hashPassword.update(req.body.password);
         hashPassword = hashPassword.digest('hex');
         //update payload.password
         payload.password = hashPassword;
         if (payload.account === undefined || payload.password === undefined) {
-            throw new Error("please fill account or password");
-        } else {
-            const result = await checkLogin(payload);
-            res.json({
-                status: "login successfully",
-                status_code: 201,
-                result: result,
-            });
+            throw {
+                message: {
+                    message: "please fill up account or password"
+                },
+                status_code: 400
+            }
         }
+        const hasUser = await checkUser(payload);
+        if (hasUser === false) {
+            throw {
+                message: {
+                    message: "account or password incorrected"
+                },
+                status_code: 400
+
+            }
+        }
+        const api_token = makeToken(20);
+        //update payload
+        payload.userID = hasUser;
+        payload.api_token = api_token;
+        const isUpdateUserToken = await updateUserToken(payload);
+        if (isUpdateUserToken === false) {
+            throw {
+                message: {
+                    message: "update user token failed"
+                },
+                status_code: 500
+            }
+        }
+        const [message, status_code] = await checkLogin(payload);
+        res.status(status_code.status_code)
+        res.json({
+            status: "login successfully",
+            result: message,
+        });
     } catch (err) {
+        const statusCode = err.status_code;
+        res.status(statusCode)
         res.json({
             status: "login failed",
-            status_code: 400,
             result: err.message,
         });
     }
